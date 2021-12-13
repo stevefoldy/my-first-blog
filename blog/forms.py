9d79afc0f0433ba05f26e4f0a209820bcb6b9748
istvanfoldy <istvanfoldy@gmail.com>
2021-12-13 23:56:12 +0100

    add Security to edit and base page

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.forms import ModelForm, TextInput, Textarea
 from .models import Post
 
 class PostForm(forms.ModelForm):
@@ -7,3 +7,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+        widgets = {
+            'title': TextInput(attrs={'class': "form-control", 'placeholder': 'Post title'}),
+            'text': Textarea(attrs={'class': "form-control", 'placeholder': 'Post description'}),
+        }
